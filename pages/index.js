b9c67a61bfed41f71901f8dcf1320b08259c16a9
Cayridd <Cayridd@gmail.com>
2025-08-04 21:42:35 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Welcome to ScrimVault Alpha</h1>
+      <p>This is your early access testing environment.</p>
+    </div>
+  );
+}
